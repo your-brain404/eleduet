e4f438d47ed4887c64d1c5a448ea17210f83f24d
Daniel <dany97971@gmail.com>
2022-09-24 21:31:19 +0200

    Zabezpieczono typy kluczy obcych w modelu ServicesServiceCategories

--- app/ServicesServiceCategories.php
+++ app/ServicesServiceCategories.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ServicesServiceCategories extends Model
 {
 	protected $guarded = ['id'];
+
+	protected $casts = [
+		'service_category_id' => 'integer',
+		'service_id' => 'integer',
+	];
 }
