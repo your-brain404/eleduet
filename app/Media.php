9bed6ca80b5c91a2bf06e59fa20cb746654b7c61
Daniel <dany97971@gmail.com>
2022-10-02 13:33:49 +0200

    Dodano skrypt aktualizujący rozmiary zdjęć

--- app/Media.php
+++ app/Media.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Media extends Model {
+class Media extends Model
+{
 	protected $table = 'media';
+	protected $guarded = ['id'];
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Mail\ReservationAccept;
 use App\Mail\ShippingConfirmation;
+use App\Media;
 use App\Reservations;
 use App\ShopOrders;
 use Illuminate\Support\Facades\Hash;
@@ -21,6 +22,15 @@ use Illuminate\Support\Facades\Hash;
 Auth::routes();
 Route::get('/test', function () {
     echo Hash::make("123eleduet123");
+});
+Route::get('/photo-sizes', function () {
+    $photos = Media::where('type', 'image/jpeg')->orWhere('type', 'image/jpg')->orWhere('type', 'image/png')->orWhere('type', 'image/bmp')->orWhere('type', 'application/octet-stream')->orWhere('type', 'image/jfif')->get();
+    for ($i = 0; $i < count($photos); $i++) {
+        $path = $_SERVER['ROOT'] . "/storage/media/{$photos[$i]->path}";
+        if (!file_exists($path)) continue;
+        list($width, $height) = getimagesize($path);
+        Media::where('id', $photos[$i]->id)->first()->fill(['width' => $width, 'height' => $height])->save();
+    }
 });
 Route::get('/auth/confirm_account/{id}', 'UsersController@confirm');
 Route::get('/{catch?}', 'HomeController@index')->name('home')->where('catch', '^(?!api).*$');
