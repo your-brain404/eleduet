1b68be837d79cfd41ea62c5162ee2c55a0b0ecdb
Daniel <dany97971@gmail.com>
2022-10-02 18:36:17 +0200

    Usunięto vue-lazyload

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ import Vuelidate from "vuelidate";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel";
 import VueMeta from "vue-meta";
-import VueLazyload from "vue-lazyload";
 import isExternal from "@/helpers/links/is-external";
 
 Vue.component("app", require("./components/App.vue").default);
@@ -17,7 +16,6 @@ Vue.use(VueAxios, axios);
 Vue.use(VueMeta, {
     refreshOnceOnNavigation: true
 });
-Vue.use(VueLazyload);
 
 Vue.prototype.$vueMeta = VueMeta;
 Vue.prototype.$isLinkExternal = isExternal;
