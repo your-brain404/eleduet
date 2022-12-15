90d0c8618756dc0c98d0655d0dcc92ca58c37c01
Daniel <dany97971@gmail.com>
2022-12-15 18:39:28 +0100

    Dodano VCard

---
+++ resources/js/components/elements/VCard.vue
@@ -0,0 +1,25 @@
+<template functional>
+  <div class="v-card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.v-card {
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  background-color: #ffffff;
+  border-color: #ffffff;
+  color: rgba(0, 0, 0, 0.87);
+  border-radius: 4px;
+  border-width: thin;
+  display: block;
+  max-width: 100%;
+  outline: none;
+  text-decoration: none;
+  transition-property: box-shadow, opacity;
+  overflow-wrap: break-word;
+  position: relative;
+  white-space: normal;
+}
+</style>
