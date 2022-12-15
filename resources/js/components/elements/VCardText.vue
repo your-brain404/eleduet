e32d309ef66dcfc1fd71d5ebf5454d3cbaa0aa4b
Daniel <dany97971@gmail.com>
2022-12-15 18:39:43 +0100

    Dodano VCardText

---
+++ resources/js/components/elements/VCardText.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <div class="v-card-text">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+.v-card-text {
+  width: 100%;
+  padding: 25px;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 1.375rem;
+  letter-spacing: 0.0071428571em;
+}
+</style>
