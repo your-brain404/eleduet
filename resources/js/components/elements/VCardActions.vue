6602dee6429d2d598fb2ad6123fa6ffd3bb86f24
Daniel <dany97971@gmail.com>
2022-12-16 23:59:41 +0100

    Dodano VCardActions

---
+++ resources/js/components/elements/VCardActions.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="v-card-actions">
+    <slot></slot>
+  </div>
+</template>
+    
+<style lang="scss">
+.v-card-actions {
+  padding: 16px;
+  align-items: center;
+  display: flex;
+}
+</style>
