695a0f01f20c86d2d3dceab832aa4a1c09251b5d
Daniel <dany97971@gmail.com>
2023-01-06 18:32:37 +0100

    Przebudowano Loader jako komponent funkcyjny

--- resources/js/components/loader/Loader.vue
+++ resources/js/components/loader/Loader.vue
@@ -1,29 +1,36 @@
-<template>
-  <v-container style="display: flex; justify-content: center; z-index: 1000000">
-    <b-spinner class="loader"></b-spinner>
-  </v-container>
+<template functional>
+  <div class="loader__container">
+    <img
+      :src="`/storage/media/${parent.$store.state.Settings.settings.li_marker}.webp`"
+      :alt="parent.$store.state.Settings.settings.li_marker_alt"
+      :width="48"
+      :height="75"
+      class="loader"
+    />
+  </div>
 </template>
 
-<script>
-import { BSpinner } from "@/plugins/bootstrap-vue/src/components/spinner/spinner";
-import VContainer from "@/components/grid/VContainer.vue";
-export default {
-  components: {
-    VContainer,
-    BSpinner,
-  },
-};
-</script>
 
-<style scoped>
-::v-deep .loader {
-  margin: 5rem;
-  padding: 5rem;
-  margin-left: auto;
-  margin-right: auto;
+
+<style lang="scss">
+.loader {
+  margin: 10rem auto;
+  animation: spin 4s infinite;
+  transform-origin: center;
+  width: auto;
+  &__container {
+    display: flex;
+    z-index: 999;
+  }
+}
+@keyframes spin {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(360deg);
+  }
 }
 </style>
 
-<style lang="scss">
-// @import "~bootstrap/scss/spinners";
-</style>
+
