85ebd9d8762062c0bca7f238e6648cd55d2272ea
Daniel <dany97971@gmail.com>
2023-07-09 16:24:40 +0200

    laravel 9 routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\api\LoginController;
+use App\Http\Controllers\Auth\api\RegisterController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\MailsController;
+use App\Http\Controllers\MediaController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -22,58 +28,54 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 foreach (DefaultCrudTables::get() as $table) {
 	$controller = ucfirst(Str::of($table)->camel()) . 'Controller';
-
-	Route::get("/$table/get_all/", "$controller@getAll");
-	Route::get("/$table/get_one/{id}", "$controller@getOne");
-	Route::put("/$table/edit", "$controller@store")->middleware(['admin']);
-	Route::post("/$table/add/", "$controller@store")->middleware(['admin']);
-	Route::delete("/$table/delete/{id}", "$controller@destroy")->middleware(['admin']);
-	Route::get("/$table/get_where/", "$controller@getWhere");
+	$namespacedController = "App\\Http\\Controllers\\$controller";
+	Route::get("/$table/get_all/", [$namespacedController, 'getAll']);
+	Route::get("/$table/get_one/{id}", [$namespacedController, 'getOne']);
+	Route::put("/$table/edit", [$namespacedController, 'store'])->middleware(['admin']);
+	Route::post("/$table/add/", [$namespacedController, 'store'])->middleware(['admin']);
+	Route::delete("/$table/delete/{id}", [$namespacedController, 'destroy'])->middleware(['admin']);
+	Route::get("/$table/get_where/", [$namespacedController, 'getWhere']);
 }
 
+Route::get(
+	'/media/get_photos/',
+	[MediaController::class, 'getPhotos']
+);
+Route::get('/media/get_files/', [MediaController::class, 'getFiles']);
 
-Route::get('/news/get_pagination', 'NewsController@getPagination');
-
-Route::get('/shop_products/get_pagination', 'ShopProductsController@getPagination');
-
-Route::get('/media/get_photos/', 'MediaController@getPhotos');
-Route::get('/media/get_files/', 'MediaController@getFiles');
-
-Route::get('/ordered_products/get_where/', 'OrderedProductsController@getWhere');
-
-Route::post('/shop_orders/shipping_confirmation/', 'ShopOrdersController@shippingConfirmation');
-
-Route::post('/avatars/add/', 'AvatarsController@store')->middleware('auth:api');
-
-Route::post('/reservations/accept/', 'ReservationsController@accept')->middleware(['admin']);
-
-Route::post('/auth/register', 'Auth\api\RegisterController@register');
-Route::post('/auth/login', 'Auth\api\LoginController@login');
-Route::post('/auth/auto_login', 'Auth\api\LoginController@autoLogin');
-
+Route::post(
+	'/auth/register',
+	[RegisterController::class, 'register']
+);
+Route::post('/auth/login', [LoginController::class, 'login']);
+Route::post(
+	'/auth/auto_login',
+	[LoginController::class, 'autoLogin']
+);
 
 
 Route::group(['middleware' => ['web']], function () {
-	Route::get('/facebook/login', 'Auth\api\LoginController@redirectToProvider');
-	Route::get('/facebook/login/callback', 'Auth\api\LoginController@handleProviderCallback');
+	Route::get('/facebook/login', [LoginController::class, 'redirectToProvider']);
+	Route::get(
+		'/facebook/login/callback',
+		[LoginController::class, 'handleProviderCallback']
+	);
 });
 
-Route::get('facebook/login/get_token', 'Auth\api\LoginController@getToken');
+Route::get('facebook/login/get_token', [LoginController::class, 'getToken']);
 
-Route::get('/gallery/get_photos/{table}/{id}', 'GalleryController@getPhotos');
+Route::get('/gallery/get_photos/{table}/{id}', [GalleryController::class, 'getPhotos']);
 
-Route::get('/users/get_commentators', 'UsersController@getCommentators');
+Route::get('/users/get_commentators', [UsersController::class, 'getCommentators']);
 
 
-Route::post('/mails/send/', 'MailsController@send');
-Route::put('/mails/answer/', 'MailsController@store')->middleware(['admin']);
+Route::post('/mails/send/', [MailsController::class, 'send']);
+Route::put('/mails/answer/', [MailsController::class, 'store'])->middleware(['admin']);
 
-Route::put('/users/edit', 'UsersController@store')->middleware(['auth:api', 'user.edit']);
-Route::post('/users/password_reminder', 'UsersController@passwordReminder');
-Route::post('/users/change_password', 'UsersController@changePassword');
+Route::put('/users/edit', [UsersController::class, 'store'])->middleware(['auth:api', 'user.edit']);
+Route::post('/users/password_reminder', [UsersController::class, 'passwordReminder']);
+Route::post('/users/change_password', [UsersController::class, 'changePassword']);
 
-Route::delete('/news_tags/delete/{id}', 'NewsTagsController@destroy')->middleware(['admin']);
-Route::get('/lucky_number/dice', 'LuckyNumberController@dice')->middleware(['admin']);
 Route::get('/cms_routes/get_all', function () {
 	return file_get_contents(__DIR__ . "/../resources/js/router/cmsRoutes.json");
 });
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
-use App\Mail\ReservationAccept;
-use App\Mail\ShippingConfirmation;
-use App\Media;
-use App\Reservations;
-use App\ShopOrders;
-use Illuminate\Support\Facades\Hash;
+use App\Models\Media;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,18 +16,18 @@ use Illuminate\Support\Facades\Hash;
 |
  */
 
-Auth::routes();
 Route::get('/test', function () {
-    echo Hash::make("123eleduet123");
+    echo HomeController::class;
 });
 Route::get('/photo-sizes', function () {
     $photos = Media::where('type', 'image/jpeg')->orWhere('type', 'image/jpg')->orWhere('type', 'image/png')->orWhere('type', 'image/bmp')->orWhere('type', 'application/octet-stream')->orWhere('type', 'image/jfif')->get();
     for ($i = 0; $i < count($photos); $i++) {
         $path = $_SERVER['ROOT'] . "/storage/media/{$photos[$i]->path}";
-        if (!file_exists($path)) continue;
+        if (!file_exists($path))
+            continue;
         list($width, $height) = getimagesize($path);
         Media::where('id', $photos[$i]->id)->first()->fill(['width' => $width, 'height' => $height])->save();
     }
 });
-Route::get('/auth/confirm_account/{id}', 'UsersController@confirm');
-Route::get('/{catch?}', 'HomeController@index')->name('home')->where('catch', '^(?!api).*$');
+Route::get('/auth/confirm_account/{id}', [UsersController::class, 'confirm']);
+Route::get('/{catch?}', [HomeController::class, 'index'])->name('home')->where('catch', '^(?!api).*$');
