07f64cb046a8c0ba0fdd52da71fc38b2d5c35b10
Daniel <dany97971@gmail.com>
2023-07-09 15:21:20 +0200

    v-model value rebuild

--- resources/js/components/forms/TinyMCE.vue
+++ resources/js/components/forms/TinyMCE.vue
@@ -1,40 +1,9 @@
 <template>
   <editor
-    v-model="value"
+    :value="value"
+    @input="$emit('input', $event)"
     api-key="m22xj0x6h1pgqb681evsy9sxvg1fgl2qktcekp6e5ef3ni2z"
-    :init="{
-      height: 500,
-      menubar: true,
-      plugins: [
-        'a11ychecker',
-        'advlist',
-        'advcode',
-        'advtable',
-        'autolink',
-        'checklist',
-        'export',
-        'lists',
-        'link',
-        'image',
-        'charmap',
-        'preview',
-        'anchor',
-        'searchreplace',
-        'visualblocks',
-        'powerpaste',
-        'fullscreen',
-        'formatpainter',
-        'insertdatetime',
-        'media',
-        'table',
-        'help',
-        'wordcount',
-      ],
-      toolbar:
-        'undo redo | casechange blocks | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
-    }"
+    :init="init"
   />
 </template>
 
@@ -45,6 +14,43 @@ export default {
     editor: Editor,
   },
   props: ["value"],
+  data() {
+    return {
+      init: {
+        height: 500,
+        menubar: true,
+        plugins: [
+          "a11ychecker",
+          "advlist",
+          "advcode",
+          "advtable",
+          "autolink",
+          "checklist",
+          "export",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "powerpaste",
+          "fullscreen",
+          "formatpainter",
+          "insertdatetime",
+          "media",
+          "table",
+          "help",
+          "wordcount",
+        ],
+        toolbar:
+          "undo redo | casechange blocks | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist checklist outdent indent | removeformat | a11ycheck code table help",
+      },
+    };
+  },
   watch: {
     value() {
       this.$emit("input", this.value);
