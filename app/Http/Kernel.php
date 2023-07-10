710202c2627df18a34fd4bffe763bdc637532028
Daniel <dany97971@gmail.com>
2023-07-10 13:07:52 +0200

    extend api throttle

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:60000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
