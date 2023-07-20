708d57a6f3f2d1ae84b3b404467e1eb8ac0ede20
Daniel <dany97971@gmail.com>
2023-07-20 11:22:37 +0200

    add svg-vue component

---
+++ resources/js/components/elements/SvgVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg-icon type="mdi" :path="path"></svg-icon>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import * as mdi from "@mdi/js";
+import ucfirst from "@/helpers/string/ucfirst.js";
+
+export default {
+  props: ["icon"],
+  components: {
+    SvgIcon,
+  },
+  computed: {
+    path() {
+      return mdi[`mdi${ucfirst(this.icon)}`];
+    },
+  },
+};
+</script>
