0f2fc9d6d9fac796d17a17e54ee1306c0015f3a8
Daniel <dany97971@gmail.com>
2023-07-30 20:58:46 +0200

    EmitDataBlockGetOne

---
+++ resources/js/components/emit-data-blocks/EmitDataBlockGetOne.vue
@@ -0,0 +1,12 @@
+<script>
+import AbstractEmitDataBlock from "./AbstractEmitDataBlock.vue";
+
+export default {
+  extends: AbstractEmitDataBlock,
+  data() {
+    return {
+      endpoint: "get_one/1",
+    };
+  },
+};
+</script>
