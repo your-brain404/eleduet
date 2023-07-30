07ac629aeb9c3dab8ce9df9b809dda2ce26d3960
Daniel <dany97971@gmail.com>
2023-07-30 20:56:04 +0200

    add CustomModal component

---
+++ resources/js/components/custom-modal/CustomModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['custom-modal', { 'custom-modal--show': modelValue }]">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: Boolean,
+  },
+};
+</script>
+
+<style lang="scss">
+.custom-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: scale(0) translate(-50%, -50%);
+  width: 80vw;
+  z-index: 100;
+  transition: 0.2s;
+  transform-origin: top left;
+  &--show {
+    transform: scale(1) translate(-50%, -50%);
+  }
+}
+</style>
