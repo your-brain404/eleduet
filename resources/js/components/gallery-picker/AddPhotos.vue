c756e884ae026077bc79f70627517b230f4300c7
Daniel <dany97971@gmail.com>
2023-08-03 11:58:28 +0200

    Correction on file extensions

--- resources/js/components/gallery-picker/AddPhotos.vue
+++ resources/js/components/gallery-picker/AddPhotos.vue
@@ -40,12 +40,12 @@ import isPhoto from "@/helpers/files/is-photo.js";
 import photoTypes from "@/helpers/files/photo-types.js";
 import VRow from "@/components/grid/VRow.vue";
 import VCol from "@/components/grid/VCol.vue";
-import VCardText from "@/components/elements/VCardText.js";
-import VCard from "@/components/elements/VCard.js";
-import VForm from "@/components/elements/VForm.js";
+import VCardText from "@/components/elements/VCardText.vue";
+import VCard from "@/components/elements/VCard.vue";
+import VForm from "@/components/elements/VForm.vue";
 import TextField from "@/components/elements/TextField.vue";
 import VBtn from "@/components/elements/VBtn.vue";
-import SvgVue from "svg-vue";
+import SvgVue from "@/components/elements/SvgVue.vue";
 export default {
   components: {
     VRow,
