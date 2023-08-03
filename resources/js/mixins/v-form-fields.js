a74f55aed0a5726ffb359647fe102e3ba2ca213f
Daniel <dany97971@gmail.com>
2023-08-03 11:53:41 +0200

    Add watchers for form-fields

--- resources/js/mixins/v-form-fields.js
+++ resources/js/mixins/v-form-fields.js
@@ -5,12 +5,25 @@ export default {
             formComponentsNames: ["text-field", "checkbox"],
         }
     },
+    methods: {
+        setFields() {
+            this.fields = Object.values(
+                this.$refs
+            ).filter((child) => {
+                return this.formComponentsNames.includes(child.$options.name);
+            })
+            this.setNewWatchers(this.fields)
+        },
+        setNewWatchers(fields) {
+            for (let i = 0; i < fields.length; i++) {
+                // works only on fields.i.x - does not work on entire object fields.i (dunno why) 
+                this.$watch(`fields.${i}.value`, () => {
+                    this.$refs.form.validate()
+                })
+            }
+        }
+    },
     mounted() {
-
-        this.fields = Object.values(
-            this.$refs
-        ).filter((child) => {
-            return this.formComponentsNames.includes(child.$options.name);
-        });
+        this.setFields()
     }
 }
