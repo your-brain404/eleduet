15d0866c5d14b2bfeebf50a62290b451145a09f3
Daniel <dany97971@gmail.com>
2023-08-03 11:54:49 +0200

    Contact and Settings extends EmitDataBlockGetOne

--- resources/js/components/emit-data-blocks/Contact.vue
+++ resources/js/components/emit-data-blocks/Contact.vue
@@ -1,43 +1,7 @@
-<template></template>
-
 <script>
-import axios from "@/plugins/axios/axios.js";
-import contactModule from "@/store/modules/contact/contactModule.js";
+import EmitDataBlockGetOne from "./EmitDataBlockGetOne.vue";
 
 export default {
-  props: ["reloadFlag"],
-  watch: {
-    reloadFlag() {
-      if (this.reloadFlag) this.getData();
-    },
-  },
-  data() {
-    return {
-      contact: {},
-    };
-  },
-  methods: {
-    getContact() {
-      this.$store.commit("loading", true);
-      axios
-        .get(`/api/contact/get_one/1`)
-        .then((res) => {
-          this.$store.commit("loading", false);
-          this.contact = [res.data];
-          this.$emit("blockDataEmit", this.contact);
-          this.$store.commit("contact", res.data);
-        })
-        .catch((err) => {
-          this.$store.commit("loading", false);
-          console.log(err);
-        });
-    },
-  },
-  created() {
-    if (!this.$store.hasModule("contact")) {
-      this.$store.registerModule("contact", contactModule);
-    }
-    this.getContact();
-  },
+  extends: EmitDataBlockGetOne,
 };
 </script>
--- resources/js/components/emit-data-blocks/Settings.vue
+++ resources/js/components/emit-data-blocks/Settings.vue
@@ -1,43 +1,7 @@
-<template></template>
-
 <script>
-import axios from "@/plugins/axios/axios.js";
-import settingsModule from "@/store/modules/settings/settingsModule.js";
+import EmitDataBlockGetOne from "./EmitDataBlockGetOne.vue";
 
 export default {
-  props: ["reloadFlag"],
-  watch: {
-    reloadFlag() {
-      if (this.reloadFlag) this.getData();
-    },
-  },
-  data() {
-    return {
-      settings: {},
-    };
-  },
-  methods: {
-    getData() {
-      this.$store.commit("loading", true);
-      axios
-        .get(`/api/settings/get_one/1`)
-        .then((res) => {
-          this.$store.commit("loading", false);
-          this.settings = res.data;
-          this.$emit("blockDataEmit", [this.settings]);
-          this.$store.commit("settings", res.data);
-        })
-        .catch((err) => {
-          this.$store.commit("loading", false);
-          console.log(err);
-        });
-    },
-  },
-  created() {
-    if (!this.$store.hasModule("settings")) {
-      this.$store.registerModule("settings", settingsModule);
-    }
-    this.getData();
-  },
+  extends: EmitDataBlockGetOne,
 };
 </script>
