3184dd3187e33aebb7c29e6a6a23d9f37d9313e7
dlewicki <dlewicki@media4u.pl>
2023-08-08 19:14:32 +0200

    Implement CustomPagination

---
+++ resources/js/components/custom-pagination/CustomPagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="custom-pagination">
+    <li role="presentation" :class="['page-item', {'disabled': value === 1}]" aria-hidden="true"><span role="menuitem" aria-label="Go to first page" aria-disabled="true" class="page-link">«</span></li>
+    <li role="presentation" :class="['page-item', {'disabled': value === 1}]" aria-hidden="true"><span role="menuitem" aria-label="Go to previous page" aria-disabled="true" class="page-link">‹</span></li>
+    <li @click="$emit('input', page)" v-for="page in pages" :key="`custom-pagination-${page}`" :class="['page-item', {'active': page === value}]">
+      <button class="page-link" >{{ page }}</button>
+    </li>
+    <li role="presentation" :class="['page-item', {'disabled': value === pages}]"><button role="menuitem" type="button" tabindex="-1" aria-label="Go to next page" class="page-link">›</button></li>
+    <li role="presentation" :class="['page-item', {'disabled': value === pages}]"><button role="menuitem" type="button" tabindex="-1" aria-label="Go to last page" class="page-link">»</button></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: Number,
+      totalRows: Number, 
+      perPage: Number
+    },
+    computed: {
+      pages() {
+        return Math.ceil(this.totalRows / this.perPage)
+      }
+    }
+  }
+</script>
