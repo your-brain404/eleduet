7073baab0b37c8f96af17ea00f4206bdcae3a8a4
dlewicki <dlewicki@media4u.pl>
2023-08-11 21:22:18 +0200

    CustomModal - v-if + transition

--- resources/js/components/custom-modal/CustomModal.vue
+++ resources/js/components/custom-modal/CustomModal.vue
@@ -1,7 +1,9 @@
 <template>
-  <div :class="['custom-modal', { 'custom-modal--show': modelValue }]">
-    <slot />
-  </div>
+  <Transition name="custom-modal-scale">
+    <div v-if="modelValue" class="custom-modal">
+      <slot />
+    </div>
+  </Transition>
 </template>
 
 <script>
@@ -13,19 +15,33 @@ export default {
 </script>
 
 <style lang="scss">
+
+.custom-modal-scale-enter-active {
+  animation: custom-modal-scale .2s;
+}
+.custom-modal-scale-leave-active {
+  animation: custom-modal-scale .2s reverse;
+}
+@keyframes custom-modal-scale {
+  0% {
+    transform: scale(0) translate(-50%, -50%);
+  }
+
+  100% {
+    transform: scale(1) translate(-50%, -50%);
+  }
+}
 .custom-modal {
   position: fixed;
   top: 50%;
   left: 50%;
-  transform: scale(0) translate(-50%, -50%);
+  transform: scale(1) translate(-50%, -50%);
   width: 80vw;
   z-index: 100;
   transition: 0.2s;
   transform-origin: top left;
   overflow-y: auto;
   height: 100%;
-  &--show {
-    transform: scale(1) translate(-50%, -50%);
-  }
+  
 }
 </style>
